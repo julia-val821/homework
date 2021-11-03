21c1b5765c965fc5625d32f8022217078152012a
JuliaVal821 <julia.kolgatina@gmail.com>
2021-11-03 17:46:06 -0400

    change correct name file

--- data/register.data.js
+++ data/register.data.js
@@ -6,7 +6,7 @@ const user = {
     firstName: "Tom",
     lastName: "Smith",
     email: email,
-    username: "TomSmith",
+    username: "ToSmith",
     password: "T1o2m3s4",
     confirmPassword: "T1o2m3s4"
 }
